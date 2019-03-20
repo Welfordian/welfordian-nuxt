133aba185c9954fc0a119c4f0d032354182b5ce0
welfordian <josh@fish.cat>
2019-03-20 17:25:22 -0400

    Adds og:typeg

--- resources/pages/_post.vue
+++ resources/pages/_post.vue
@@ -69,20 +69,13 @@
                     { name: 'og:url', content: `https://welford.me/${this.post.slug}` },
                     { name: 'og:title', content: this.post.title },
                     { name: 'og:description', content: this.post.intro_text },
-                    { name: 'og:image', content: this.post.intro_image }
+                    { name: 'og:image', content: this.post.intro_image },
+                    { name: 'og:type', content: 'article' }
                 ]
             }
         }
     }
 </script>
-
-<meta name="twitter:card" content="summary" />
-<meta name="twitter:site" content="@nytimesbits" />
-<meta name="twitter:creator" content="@nickbilton" />
-<meta property="og:url" content="http://bits.blogs.nytimes.com/2011/12/08/a-twitter-for-my-sister/" />
-<meta property="og:title" content="A Twitter for My Sister" />
-<meta property="og:description" content="In the early days, Twitter grew so quickly that it was almost impossible to add new features because engineers spent their time trying to keep the rocket ship from stalling." />
-<meta property="og:image" content="http://graphics8.nytimes.com/images/2011/12/08/technology/bits-newtwitter/bits-newtwitter-tmagArticle.jpg" />
 
 <style type="less">
   pre {
