b6446330b6ddc7c96fe3851f094fc5d907e5b101
welfordian <josh@fish.cat>
2019-03-20 17:36:23 -0400

    Remove duplicate meta

--- resources/pages/_post.vue
+++ resources/pages/_post.vue
@@ -63,14 +63,14 @@
         head() {
             return {
                 meta: [
-                    { name: 'twitter:card', property: 'twitter:card', content: 'summary' },
-                    { name: 'twitter:site', property: 'twitter:site', content: '@welfordian' },
-                    { name: 'twitter:creator', property: 'twitter:creator', content: '@welfordian' },
-                    { name: 'og:url', property: 'og:url', content: `https://welford.me/${this.post.slug}` },
-                    { name: 'og:title', property: 'og:title', content: this.post.title },
-                    { name: 'og:description', property: 'og:description', content: this.post.intro_text },
-                    { name: 'og:image', property: 'og:image', content: this.post.intro_image },
-                    { name: 'og:type', property: 'og:type', content: 'article' }
+                    { name: 'twitter:card', hid: 'twitter:card', content: 'summary' },
+                    { name: 'twitter:site', hid: 'twitter:site', content: '@welfordian' },
+                    { name: 'twitter:creator', hid: 'twitter:creator', content: '@welfordian' },
+                    { name: 'og:url', hid: 'og:url', content: `https://welford.me/${this.post.slug}` },
+                    { name: 'og:title', hid: 'og:title', content: this.post.title },
+                    { name: 'og:description', hid: 'og:description', content: this.post.intro_text },
+                    { name: 'og:image', hid: 'og:image', content: this.post.intro_image },
+                    { name: 'og:type', hid: 'og:type', content: 'article' }
                 ]
             }
         }
